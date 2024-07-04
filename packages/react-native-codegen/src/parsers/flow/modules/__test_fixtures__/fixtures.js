e2402e42bd339261ac3f62f2ec52162c650dd815
Christoph Purrer <chpurrer@meta.com>
2024-07-03 21:31:51 -0700

    Add test fixture for function property types
    Summary:
    This feature was added in https://github.com/facebook/react-native/pull/41810
    This change adds a test fixture for it and updates the UT snapshot
    
    Changelog: [Internal]
    
    Reviewed By: rshest
    
    Differential Revision: D56739139

--- packages/react-native-codegen/src/parsers/flow/modules/__test_fixtures__/fixtures.js
+++ packages/react-native-codegen/src/parsers/flow/modules/__test_fixtures__/fixtures.js
@@ -774,6 +774,13 @@ export type CustomDeviceEvent = {
   degree?: number,
 };
 
+export type MenuItem = {
+  label: string,
+  onPress: (value: string, flag: boolean) => void,
+  shortcut?: string,
+  items?: Array<MenuItem>,
+};
+
 export interface Spec extends TurboModule {
   +getCallback: () => () => void;
   +getMixed: (arg: mixed) => mixed;
@@ -783,6 +790,7 @@ export interface Spec extends TurboModule {
   +getMap: (arg: {[a: string]: ?number}) => {[b: string]: ?number};
   +getAnotherMap: (arg: {[string]: string}) => {[string]: string};
   +getUnion: (chooseInt: ChooseInt, chooseFloat: ChooseFloat, chooseObject: ChooseObject, chooseString: ChooseString) => ChooseObject;
+  +setMenu: (menuItem: MenuItem) => void;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('SampleTurboModuleCxx');
